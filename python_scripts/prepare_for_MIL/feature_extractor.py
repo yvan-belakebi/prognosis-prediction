fc9d04e171877820a8ed41462e79890a88851276
yvan-belakebi <yvan.bel.jo@gmail.com>
2026-04-16 12:37:38 +0200

    bit of readability

--- python_scripts/prepare_for_MIL/feature_extractor.py
+++ python_scripts/prepare_for_MIL/feature_extractor.py
@@ -79,9 +79,9 @@ def infer_on_image(model, transform, image_path):
     image = Image.open(image_path)
     image = transform(image).unsqueeze(
         dim=0
-    )  # Image (torch.Tensor) with shape [1, 3, 224, 224] following image resizing and normalization (ImageNet parameters)
+    )  # Image (torch.Tensor) with shape [1, 3, 224, 224] following image resizing and normalization
     with torch.inference_mode():
         feature_emb = model(
             image
-        )  # Extracted features (torch.Tensor) with shape [1,1536]
+        )  # Extracted features (torch.Tensor) with shape [1,1536] for example
     return feature_emb
