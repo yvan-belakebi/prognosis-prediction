e2ad9a704a9729ae06bdd4d35014eb4473fc7328
yvan-belakebi <yvan.bel.jo@gmail.com>
2026-04-28 09:12:59 +0200

    included Virchow2 and h-optimus-1 to loading for offline use

--- python_scripts/prepare_for_MIL/download_weights.py
+++ python_scripts/prepare_for_MIL/download_weights.py
@@ -3,21 +3,45 @@ from huggingface_hub import login, hf_hub_download
 
 login()
 
-local_dir = "./uni2h_model"
-os.makedirs(local_dir, exist_ok=True)
+models = {
+    "uni2h": "MahmoodLab/UNI2-h",
+    "virchow2": "paige-ai/Virchow2",
+    "hoptimus1": "bioptimus/H-optimus-1",
+}
 
-hf_hub_download(
-    repo_id="MahmoodLab/UNI2-h",
-    filename="pytorch_model.bin",
-    local_dir=local_dir,
-    force_download=True,
-)
+for name, repo in models.items():
 
-hf_hub_download(
-    repo_id="MahmoodLab/UNI2-h",
-    filename="config.json",
-    local_dir=local_dir,
-    force_download=True,
-)
+    local_dir = f"./models/{name}"
+    os.makedirs(local_dir, exist_ok=True)
 
-print("Download complete.")
+    try:
+        hf_hub_download(
+            repo_id=repo,
+            filename="pytorch_model.bin",
+            local_dir=local_dir,
+            force_download=True,
+        )
+    except:
+        pass
+
+    try:
+        hf_hub_download(
+            repo_id=repo,
+            filename="model.safetensors",
+            local_dir=local_dir,
+            force_download=True,
+        )
+    except:
+        pass
+
+    try:
+        hf_hub_download(
+            repo_id=repo,
+            filename="config.json",
+            local_dir=local_dir,
+            force_download=True,
+        )
+    except:
+        pass
+
+print("All model files downloaded.")
--- python_scripts/prepare_for_MIL/load_feature_extractors_offline.py
+++ python_scripts/prepare_for_MIL/load_feature_extractors_offline.py
@@ -3,11 +3,15 @@ import torch
 import timm
 from torchvision import transforms
 
+############################################
+# 1. UNI2-h
+############################################
+
 
 def load_uni2h_feature_extractor():
-    model_dir = "./uni2h_model"
+    uni_dir = "./models/uni2h"
 
-    timm_kwargs = {
+    uni_kwargs = {
         "model_name": "vit_giant_patch14_224",
         "img_size": 224,
         "patch_size": 14,
@@ -24,15 +28,17 @@ def load_uni2h_feature_extractor():
         "dynamic_img_size": True,
     }
 
-    model = timm.create_model(pretrained=False, **timm_kwargs)
+    uni_model = timm.create_model(pretrained=False, **uni_kwargs)
 
-    weights = torch.load(
-        os.path.join(model_dir, "pytorch_model.bin"), map_location="cpu"
+    uni_weights = torch.load(
+        os.path.join(uni_dir, "pytorch_model.bin"), map_location="cpu"
     )
 
-    model.load_state_dict(weights, strict=True)
+    uni_model.load_state_dict(uni_weights, strict=True)
+    uni_model.eval()
 
-    transform = transforms.Compose(
+    # UNI2-h transform
+    transform_uni = transforms.Compose(
         [
             transforms.Resize(224),
             transforms.CenterCrop(224),
@@ -40,6 +46,70 @@ def load_uni2h_feature_extractor():
             transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
         ]
     )
+    return uni_model, transform_uni
 
-    model.eval()
-    return model, transform
+
+############################################
+# 2. VIRCHOW2
+############################################
+
+
+def load_virchow2_feature_extractor():
+    virchow_dir = "./models/virchow2"
+
+    virchow_model = timm.create_model(
+        "vit_large_patch14_224", pretrained=False, num_classes=0
+    )
+
+    virchow_weights = torch.load(
+        os.path.join(virchow_dir, "pytorch_model.bin"), map_location="cpu"
+    )
+
+    virchow_model.load_state_dict(virchow_weights, strict=False)
+    virchow_model.eval()
+
+    # Virchow2 transform
+    transform_virchow = transforms.Compose(
+        [
+            transforms.Resize(224),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        ]
+    )
+    return virchow_model, transform_virchow
+
+
+############################################
+# 3. H-OPTIMUS-1
+############################################
+
+
+def load_hoptimus1_feature_extractor():
+    hopt_dir = "./models/hoptimus1"
+
+    hoptimus_model = timm.create_model(
+        "vit_large_patch14_224",
+        pretrained=False,
+        num_classes=0,
+        init_values=1e-5,
+        dynamic_img_size=False,
+    )
+
+    hopt_weights = torch.load(
+        os.path.join(hopt_dir, "pytorch_model.bin"), map_location="cpu"
+    )
+
+    hoptimus_model.load_state_dict(hopt_weights, strict=False)
+    hoptimus_model.eval()
+
+    # H-optimus-1 transform
+    transform_hoptimus = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean=(0.707223, 0.578729, 0.703617), std=(0.211883, 0.230117, 0.177517)
+            ),
+        ]
+    )
+    return hoptimus_model, transform_hoptimus
