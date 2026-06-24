8d30841ee0ea7b9b9f7e7250bbb56e651f08ef8a
yvan-belakebi <yvan.bel.jo@gmail.com>
2026-06-24 14:49:02 +0200

    refactoring in followup data

--- followup_data/labels_combined_to_labels.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("labels_combined.csv", encoding="utf-8")
-df = df[df["time"] >= 730]
-df = df[df["stain"] == "PAS"]
-df[["file_name", "time", "stain", "event", "source"]].to_csv(
-    "labels.csv", index=False, encoding="utf-8"
-)
