4e4473d644cabc155fc72c1ecf750e8f3b27f69c
yvan-belakebi <yvan.bel.jo@gmail.com>
2026-08-11 10:43:04 +0200

    deleted outdated and one-off scripts

--- patient_lists/patient_format.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from itables import init_notebook_mode
-
-init_notebook_mode(connected=True)
-
-df = pd.read_csv("patient_list.csv", dtype={"PersNummer": str})
-df["PersNummer"] = df["PersNummer"].astype(str).str.zfill(11)
-df.to_csv("patient_list_with_leading_zeros.csv", index=False, encoding="utf-8")
